7e501c1f3197be8f19ab47e80e59f430866f1ad2
cheripai <datdo@rocketmail.com>
2014-12-19 20:05:11 -0800

    startpage prototype

---
+++ javascript/script.js
@@ -0,0 +1,54 @@
+$(document).ready(function() {
+    var wid = '5389213', temp, condition;
+    $(".menu").slideToggle();
+    $.ajax({
+        url: 'http://api.openweathermap.org/data/2.5/weather?id=' + wid, 
+        dataType: 'json',
+        success: function(data) {
+           temp = data['main']['temp'];
+           condition = data['weather'][0]['id'];
+           $('#Temp').prepend(convert_temp(temp));
+           document.getElementById("icon").src = get_icon(condition);
+        },
+         error: function() {
+            alert('error');
+        }
+    });
+});
+
+
+function convert_temp(temp)
+{
+    return Math.round((temp - 273.15) * 1.8 + 32);
+}
+
+function get_icon(weather_code)
+{
+    if(weather_code < 300)
+        //thunderstorms
+        return 'images/cloud310.png';
+    else if(weather_code < 500)
+        //rain
+        return 'images/rain4.png';
+    else if(weather_code < 600)
+        //snow
+        return 'images/cloud324.png';
+    else if(weather_code < 700)
+        //weird stuff FIXME
+        return 'images/sun94.png';
+    else if(weather_code == 800)
+        //sunny
+        return 'images/sun94.png';
+    else if(weather_code < 900)
+        //clouds
+        return 'images/cloudy19.png';
+    else if(weather_code != 903 && weather_code != 904)
+        //windy
+        return 'images/winds2.png';
+    else if(weather_code == 906)
+        //hail
+        return 'images/cloud324.png';
+    else
+        //sunny
+        return 'images/sun94.png';
+}
